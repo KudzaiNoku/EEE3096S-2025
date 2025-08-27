c1749761a5cd0e02766b6332aafe7cbb1255c73d
Kudzai <nykkud006@myuct.ac.za>
2025-08-27 14:41:24 +0200

    completed tasks 3, for and 5

--- Practical2/Core/Src/assembly.s
+++ Practical2/Core/Src/assembly.s
@@ -36,6 +36,22 @@ ASM_Main:
 @ TODO: Add code, labels and logic for button checks and LED patterns
 
 main_loop:
+	SW3_pressed:
+		@check if the button SW3 is pressed
+		LDR R5, [R0, #0x10]
+		MOVS R7, #8 @ 8=0b0000 1000
+		ANDS R5, R7 @isolate PA3 bit
+		@if pressed, keep in this loop till its released
+		CMP R5, #0
+		BEQ SW3_pressed
+
+	@check if the button SW2 is pressed
+	LDR R5, [R0, #0x10]
+	MOVS R7, #4 @ 4=0b0000 0100
+	ANDS R5, R7 @isolate PA2 bit
+	@if SW2 has been pressed, go to buttonSW2_pressed until button is no longer pressed
+	CMP R5, #0
+	BEQ buttonSW2_pressed
 
 	@ read ODR into temp register R3
 	LDR R3, [R1, #0x14]
@@ -73,13 +89,45 @@ main_loop:
 	@B main_loop
 
 
+buttonSW2_pressed:
+	@MOV R5, #0xAA
+	@STR R5, STR R3, [R1, #0x14]
+
+    LDR R3, [R1, #0x14] @ read GPIOB->ODR
+    BICS R3, R4 @ clear low 8
+    MOVS R5, #0xAA
+    ORRS R3, R5 @ insert 0xAA
+    STR  R3, [R1, #0x14]      @ write ODR
+
+hold_it_right_there:
+	BL delay
+
+    @check SW2 again and stay here while still pressed
+    LDR   R5, [R0, #0x10]@ IDR
+    MOVS  R7, #4  @ mask PA2
+    ANDS  R5, R7
+    CMP   R5, #0
+    BEQ   hold_it_right_there  @keep forcing 0xAA if still pressed
+
+	B main_loop
 
 
 @ --- delay subroutine: ~0.7 s if LONG_DELAY_CNT ≈ 1400000 ---
 @ uses R6 as the loop counter
 @ clobbers: R6
 delay:
-    LDR   R6, LONG_DELAY_CNT   @ load the literal value (from .word below)
+	@get idr register value
+	LDR R5, [R0, #0x10]
+	@use bitmask to check value of PA1
+	MOVS R3, #2 @ #2 = 0b00000010
+	ANDS R5, R3 @ isolate PA1 bit
+	@ if bit of PA1 = 0, button has been pressed, R6 = short_delay_cnt
+	CMP R5, #0
+	BEQ short_delay
+	LDR R6, LONG_DELAY_CNT
+
+	short_delay:
+    	LDR   R6, SHORT_DELAY_CNT   @ load the literal value (from .word below)
 delay_loop:
     SUBS  R6, #1               @ 1 cycle
     BNE   delay_loop           @ ~3 cycles when taken, 1 when not
@@ -94,5 +142,5 @@ GPIOB_BASE:  		.word 0x48000400
 MODER_OUTPUT: 		.word 0x5555
 
 @ TODO: Add your own values for these delays
-LONG_DELAY_CNT: 	.word 1400000
-SHORT_DELAY_CNT: 	.word 0
+LONG_DELAY_CNT: 	.word 1400000   @ ~0.7 (8MHz * 0.7/ 4 cycles)
+SHORT_DELAY_CNT: 	.word 600000    @ ~0.3 s (8MHz * 0.3 / 4 cycles)
